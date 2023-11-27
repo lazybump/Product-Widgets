158b10b530ffcf6b573fe6758f95ec371f4a739c
lazybump <104283344+lazybump@users.noreply.github.com>
2023-11-27 12:20:54 +0000

    adjust tooltip font size

--- src/components/Tooltip.tsx
+++ src/components/Tooltip.tsx
@@ -24,18 +24,21 @@ const Tooltip = () => {
     >
       <IoMdInformationCircleOutline className="w-3 h-3" />
       <div
-        className={`absolute -top-0 -translate-x-1/2 -translate-y-full w-44 bg-white shadow-2xl rounded-lg p-4 text-[9px] text-center transition-all duration-300 ${
+        className={`w-60 h-40 rounded-md p-4 space-y-3 bg-white shadow-md  absolute -top-0 -translate-x-1/2 -translate-y-full transition-all duration-300 ${
           isTooltipOpen
             ? "fade-in pointer-events-auto"
             : "fade-out pointer-events-none"
         }`}
       >
-        <p className="text-black">
+        <p className="text-sm text-center text-black">
           This widget links directly to your public profile so that you can
           easily share your impact with your customers. Turn it off here if you
           do not want the badge to link to it.
         </p>
-        <a href="#" className="block mt-3 text-blue-500 font-bold">
+        <a
+          href="#"
+          className="font-bold text-sm inline-block w-full text-center"
+        >
           View Public Profile
         </a>
       </div>
